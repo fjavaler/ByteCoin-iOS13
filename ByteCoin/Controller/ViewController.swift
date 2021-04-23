061e7d0eb3335bfda4324a1481411a9a20ee4f36
Frederick Javalera <frederickjavalera@gmail.com>
2021-04-22 19:22:31 -0600

    - Add IBOutlets in view controller. - Add API key computed property and getCoinPrice stub to model.

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+  
+  let coinManager = CoinManager()
+  
+  // Number of columns to display in picker view.
+  func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    return 1
+  }
+  
+  // Number of rows to display in picker view.
+  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    return coinManager.currencyArray.count
+  }
+  
+  // Sets name of row
+  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    return coinManager.currencyArray[row]
+  }
+  
+  // Gets the coin price for the currency type
+  func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    coinManager.getCoinPrice(for: coinManager.currencyArray[row])
+  }
+  
+  @IBOutlet weak var bitcoinLabel: UILabel!
+  @IBOutlet weak var currencyLabel: UILabel!
+  @IBOutlet weak var currencyPicker: UIPickerView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    currencyPicker.dataSource = self
+    currencyPicker.delegate = self
+  }
+  
 }
-
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -9,11 +9,35 @@
 import Foundation
 
 struct CoinManager {
+  
+  let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+  
+  // Computed property for API Key.
+  private var apiKey: String {
+    get {
+      // Get filePath of hidden .plist file.
+      guard let filePath = Bundle.main.path(forResource: "CoinAPI", ofType: "plist") else {
+        fatalError("Couldn't find file 'CoinAPI.plist'.")
+      }
+      
+      // Store contents of file in a variable as an dictionary.
+      let plist = NSDictionary(contentsOfFile: filePath)
+      
+      // Retrieve value associated with key in dictionary and store.
+      guard let value = plist?.object(forKey: "API_KEY") as? String else {
+        fatalError("Couldn't find key 'API_KEY' in 'CoinAPI.plist'.")
+      }
+      
+      // return the value.
+      return value
+    }
+  }
+  
+  func getCoinPrice(for currency: String) {
     
-    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
-    
-    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-
-    
+  }
+  
+  let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+  
+  
 }
